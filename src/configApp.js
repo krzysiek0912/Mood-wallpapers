f53303ef91e08b988c095539f2a8c4f2d5f36a82
Krzysztof Branecki <krzysiek091290@gmail.com>
2020-04-09 18:20:46 +0200

    Add engines version to push heroku server

--- src/configApp.js
+++ src/configApp.js
@@ -1,2 +1,2 @@
 export const baseURL = process.env.REACT_APP_BASE_URL;
-export const accessKey = process.env.REACT_APP_ACCESS_KEY;
+export const accessKey = 'NHymEM6xDbhMf9qnaTIIKSYJPhROQmqzJbvfkvAhdbM';
